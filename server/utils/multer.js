8f354daa6e3d885c9c2e3e1a0b013ac326bd8ce6
Samar Abbas <samarabbas172003@gmail.com>
2025-02-10 16:53:24 +0530

    Full Project25

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -3,17 +3,14 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 
-// Handle __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Ensure the 'uploads' folder exists
 const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-// Multer Storage Configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadDir);
@@ -23,7 +20,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// File Filter (Optional) - to accept only specific file types
 const fileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png|gif/;
   const extname = allowedTypes.test(
@@ -38,10 +34,9 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// Multer Upload Middleware
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: fileFilter,
 });
 
